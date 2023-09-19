192eef6165b354a0fd9bdf3d32e930c9589fcd5f
hyunmo <jrsky723@gmail.com>
2023-09-19 19:58:44 +0900

    # 18.0 Header
    - Chakra-UI를 최대한 많이 활용해보자.
    - Box : div
    - React Icons : react에서 사용할 수 있는 아이콘 라이브러리
    - 설치하면, 모든 아이콘을 사용할 수 있다.
    - Hstack은 수평으로 정렬된 컴포넌트를 보여준다.
      - justify 속성을 사용하여, 컴포넌트를 어디에 정렬할 것인지 설정할 수 있다.
      - 왜냐하면 hstack은 flexbox이기 때문이다.
    - 아이콘을 박스로 감싸서 색을 변경시키는 것이 좋다.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,25 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
 export default function Root() {
   return (
-    <h1>
-      Im root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={10}
+        px={5}
+        borderBottomWidth={1}
+      >
+        <Box color={"red.500"}>
+          <FaAirbnb size={48} />
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
