f77bef57a1cce3e4f03cf7f46266f813a2d77957
hyunmo <jrsky723@gmail.com>
2023-09-20 19:30:48 +0900

    # 18.5 Responsive Design
    - 반응형 디자인
    - chakra-ui의 모든 prop은 반응형 디자인을 적용할 수 있다.
    - chakra-ui는 모바일을 기본으로 한다.
    - HStack 과 VStack은 방향을 바꿀 수 없기 때문에, 어떤 방향으로 정렬할지 확실할 때 사용한다.
      - Stack을 사용

---
+++ src/components/Room.tsx
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FaRegHeart, FaStar } from "react-icons/fa";
+
+export default function Room() {
+  const gray = useColorModeValue("gray.600", "gray.300");
+  return (
+    <VStack alignItems={"flex-start"}>
+      <Box position={"relative"} overflow={"hidden"} mb={3} rounded="3xl">
+        <Image
+          minH="280"
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-13903824/original/82d996fb-d7c4-46a8-a713-febd281cd69f.jpeg?im_w=320"
+        />
+        <Button
+          variant={"unstyled"}
+          position="absolute"
+          top={0}
+          right={0}
+          color="white"
+        >
+          <FaRegHeart size="20px" />
+        </Button>
+      </Box>
+      <Box>
+        <Grid gap={2} templateColumns={"6fr 1fr"}>
+          <Text display={"block"} as="b" noOfLines={1} fontSize="md">
+            Torino, Piemonte, Italy
+          </Text>
+          <HStack spacing={1}>
+            <FaStar size={15} />
+            <Text>5.0</Text>
+          </HStack>
+        </Grid>
+        <Text fontSize={"sm"} color={gray}>
+          Seoul, S. Korea
+        </Text>
+      </Box>
+      <Text fontSize={"sm"} color={gray}>
+        <Text as="b">$72</Text> / night
+      </Text>
+    </VStack>
+  );
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,48 +1,30 @@
-import {
-  Box,
-  Grid,
-  Heading,
-  Image,
-  VStack,
-  Text,
-  HStack,
-} from "@chakra-ui/react";
-import { FaStar } from "react-icons/fa";
+import { Grid } from "@chakra-ui/react";
+import Room from "../components/Room";
 
 export default function Home() {
   return (
     <Grid
       mt={10}
-      px={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
       columnGap={4}
       rowGap={8}
-      templateColumns={"repeat(5, 1fr)"}
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
     >
-      <VStack alignItems={"flex-start"}>
-        <Box overflow={"hidden"} mb={3} rounded="3xl">
-          <Image
-            h="280"
-            src="https://a0.muscache.com/im/pictures/miso/Hosting-13903824/original/82d996fb-d7c4-46a8-a713-febd281cd69f.jpeg?im_w=320"
-          />
-        </Box>
-        <Box>
-          <Grid gap={2} templateColumns={"6fr 1fr"}>
-            <Text as="b" noOfLines={1} fontSize="md">
-              Torino, Piemonte, Italy
-            </Text>
-            <HStack spacing={1}>
-              <FaStar size={15} />
-              <Text>5.0</Text>
-            </HStack>
-          </Grid>
-          <Text fontSize={"sm"} color="gray.600">
-            Seoul, S. Korea
-          </Text>
-        </Box>
-        <Text fontSize={"sm"} color="gray.600">
-          <Text as="b">$72</Text> / night
-        </Text>
-      </VStack>
+      {[
+        1, 2, 3, 4, 56, 7, 7, 7, 7, 7, 7, 7, 1, 32, 2, 4, 3, 4, 1, 32, 4, 3, 2,
+        5,
+      ].map((index) => (
+        <Room key={index} />
+      ))}
     </Grid>
   );
 }
