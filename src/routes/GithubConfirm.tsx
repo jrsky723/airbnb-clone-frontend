c556237bd078ba0ef349e8b0f29cc93031001438
hanu607 <jrsky723@gmail.com>
2023-10-02 09:55:44 +0900

    # 20.16 Code Challenge
    - Sinup
    - GitHub Confirm Refactoring
    - Kakao Confirm Refactoring

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -2,27 +2,31 @@ import { Heading, Spinner, Text, VStack, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { githubLogIn } from "../api";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function GithubConfirm() {
   const { search } = useLocation();
   const toast = useToast();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const mutation = useMutation(githubLogIn, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Welcome!",
+        description: "Happy to have you back!",
+      });
+
+      queryClient.refetchQueries(["me"]);
+
+      navigate("/");
+    },
+  });
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     if (code) {
-      const status = await githubLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!",
-          description: "Happy to have you back!",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
+      mutation.mutate(code);
     }
   };
   useEffect(() => {
--- src/routes/KakaoConfirm.tsx
+++ src/routes/KakaoConfirm.tsx
@@ -2,28 +2,32 @@ import { Heading, Spinner, Text, VStack, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { kakaoLogIn } from "../api";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function KakaoConfirm() {
   const { search } = useLocation();
   const toast = useToast();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const mutation = useMutation(kakaoLogIn, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Welcome!",
+        description: "Happy to have you back!",
+      });
+
+      queryClient.refetchQueries(["me"]);
+
+      navigate("/");
+    },
+  });
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     console.log(code);
     if (code) {
-      const status = await kakaoLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!",
-          description: "Happy to have you back!",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
+      mutation.mutate(code);
     }
   };
   useEffect(() => {
