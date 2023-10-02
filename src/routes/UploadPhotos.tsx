5dc0901274fc2e121b7ff889e4e2ac621db23949
hanu607 <jrsky723@gmail.com>
2023-10-02 22:04:07 +0900

    # 21.8 Final Mutation
    - 이제 얻은 cloudflare url을 backend의 photo object로 저장한다.
    - 이제 photo url을 통해, 해당 방에 image를 보여줄 수 있다.

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -6,14 +6,14 @@ import {
   Heading,
   Input,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
-import { useParams } from "react-router-dom";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
 import { useMutation } from "@tanstack/react-query";
-import { getUploadURL, uploadImage } from "../api";
-import { watch } from "fs";
+import { createPhoto, getUploadURL, uploadImage } from "../api";
+import { useParams } from "react-router-dom";
 
 interface IForm {
   file: FileList;
@@ -25,10 +25,29 @@ interface IUploadURLResponse {
 }
 
 export default function UploadPhotos() {
-  const { register, handleSubmit, watch } = useForm<IForm>();
+  const { register, handleSubmit, watch, reset } = useForm<IForm>();
+  const { roomPk } = useParams();
+  const toast = useToast();
+  const createPhotoMutation = useMutation(createPhoto, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Image uploaded!",
+        isClosable: true,
+        description: "Feel free to upload more images",
+      });
+      reset();
+    },
+  });
   const uploadImageMutation = useMutation(uploadImage, {
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: ({ result }: any) => {
+      if (roomPk) {
+        createPhotoMutation.mutate({
+          description: "I love react",
+          file: `https://imagedelivery.net/EahtQDXa4ejSdZRpC0xmZg/${result.id}/public`,
+          roomPk,
+        });
+      }
     },
   });
   const uploadURLMutation = useMutation(getUploadURL, {
@@ -64,7 +83,16 @@ export default function UploadPhotos() {
             <FormControl>
               <Input {...register("file")} type="file" accept="image/*" />
             </FormControl>
-            <Button type="submit" w="full" colorScheme={"red"}>
+            <Button
+              isLoading={
+                createPhotoMutation.isLoading ||
+                uploadImageMutation.isLoading ||
+                uploadURLMutation.isLoading
+              }
+              type="submit"
+              w="full"
+              colorScheme={"red"}
+            >
               Upload photos
             </Button>
           </VStack>
